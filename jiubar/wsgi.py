c93e52ae5de64fe3e8edb7849175c48f0cdd2af3
Dylan Luc <nguyd.luc@gmail.com>
2024-02-19 22:11:17 -0700

    update wsgi

--- jiubar/wsgi.py
+++ jiubar/wsgi.py
@@ -13,6 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jiubar.settings")
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
 
-app = application
+# app = application
+
+app = get_wsgi_application()
