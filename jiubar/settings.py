ddd92ef01c9945e9eb543eb0e48dc2960acbf75d
Dylan Luc <nguyd.luc@gmail.com>
2024-02-19 21:24:51 -0700

    Updated database

--- jiubar/settings.py
+++ jiubar/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = "jiubar.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "jiubar",
+        "PASSWORD": "Jiubar123",
+        "HOST": "jiubar.cn2e4iy26dk1.us-east-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
 
